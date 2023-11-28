490604d18d44c185fcb3479ab7cdce4e9a3ba1b9
Brageskrettingland <brageskrettingland@gmail.com>
2023-11-28 12:04:21 +0100

    nyeste

--- ball_crontol_servo.py
+++ ball_crontol_servo.py
@@ -1,31 +1,19 @@
 import multiprocessing as mp
 from multiprocessing import Queue
 import cvzone
+import numpy as np
 from cvzone.ColorModule import ColorFinder
 import cv2
 import serial
-import math
-from tkinter import *
-import numpy as np
 import time
+from tkinter import *
 
-# -------------------------------------------Both programs(Servo Control and Ball Tracker) in one -------------------------------------------
-"""
-For running both programs simultaneously we can use multithreading or multiprocessing
-"""
-
-camera_port = 0
-cap = cv2.VideoCapture(camera_port)
-cap.set(3, 1280)
-cap.set(4, 720)
-
-get, img = cap.read()
-h, w, _ = img.shape
-
-port_id = '/dev/cu.usbmodem21101'
-# initialise serial interface
-arduino = serial.Serial(port=port_id, baudrate=250000, timeout=0.1)
-
+hmin_v = 10
+hmax_v = 30
+smin_v = 170
+smax_v = 255
+vmin_v = 200
+vmax_v = 255
 # define servo angles and set a value
 servo1_angle = 0
 servo2_angle = 0
@@ -33,28 +21,37 @@ servo3_angle = 0
 all_angle = 0
 
 # Set a limit to upto which you want to rotate the servos (You can do it according to your needs)
-servo1_angle_limit_positive = 90
-servo1_angle_limit_negative = -90
+servo1_angle_zero = 11.2
+servo1_angle_limit_positive = 40
+servo1_angle_limit_negative = -50
 
-servo2_angle_limit_positive = 90
-servo2_angle_limit_negative = -90
+servo2_angle_zero = -9.3
+servo2_angle_limit_positive = 30
+servo2_angle_limit_negative = -73
 
-servo3_angle_limit_positive = 90
-servo3_angle_limit_negative = -90
-
-
-
+servo3_angle_zero = 0
+servo3_angle_limit_positive = 40
+servo3_angle_limit_negative = -53
+camera_port = 0
+cap = cv2.VideoCapture(camera_port)
+cap.set(3, 1280)
+cap.set(4, 720)
+get, img = cap.read()
+h, w, _ = img.shape
 
 def ball_track(key1, queue):
+
+
 
     if key1:
         print('Ball tracking is initiated')
 
-    myColorFinder = ColorFinder(False)  # if you want to find the color and calibrate the program we use this *(Debugging)
-    hsvVals = {'hmin': 10, 'smin': 170, 'vmin': 200, 'hmax': 30, 'smax': 255, 'vmax': 255}
+    myColorFinder = ColorFinder(
+        False)  # if you want to find the color and calibrate the program we use this *(Debugging)
+    hsvVals = {'hmin': hmin_v, 'smin': smin_v, 'vmin': vmin_v, 'hmax': hmax_v, 'smax': smax_v,
+               'vmax': vmax_v}
 
-    center_point = [626, 337, 2210] # center point of the plate, calibrated
-
+    center_point = [640, 360, 2210]  # center point of the plate, calibrated
 
     while True:
         get, img = cap.read()
@@ -62,81 +59,31 @@ def ball_track(key1, queue):
         imgContour, countours = cvzone.findContours(img, mask)
 
         if countours:
-
             data = round((countours[0]['center'][0] - center_point[0]) / 10), \
-                   round((h - countours[0]['center'][1] - center_point[1]) / 10), \
-                   round(int(countours[0]['area'] - center_point[2])/100)
+                round((h - countours[0]['center'][1] - center_point[1]) / 10), \
+                round(int(countours[0]['area'] - center_point[2]) / 100)
 
             queue.put(data)
-            print("The got coordinates for the ball are :", data)
-
         else:
-            data = 'nil' # returns nil if we cant find the ball
+            data = 'nil'  # returns nil if we cant find the ball
             queue.put(data)
 
         imgStack = cvzone.stackImages([imgContour], 1, 1)
-        # imgStack = cvzone.stackImages([img,imgColor, mask, imgContour],2,0.5) #use for calibration and correction
+        # mgStack = cvzone.stackImages([img, imgColor, mask, imgContour], 2, 0.5)  # use for calibration and correction
         cv2.imshow("Image", imgStack)
         cv2.waitKey(1)
 
 
 def servo_control(key2, queue):
+    port_id = '/dev/cu.usbmodem21101'
+    # initialise serial interface
+    arduino = serial.Serial(port=port_id, baudrate=250000, timeout=0.1)
+
     if key2:
         print('Servo controls are initiated')
 
-
-    def all_angle_assign(angle_passed1,angle_passed2,angle_passed3):
-        global servo1_angle, servo2_angle, servo3_angle
-        servo1_angle = math.radians(float(angle_passed1))
-        servo2_angle = math.radians(float(angle_passed2))
-        servo3_angle = math.radians(float(angle_passed3))
-        write_servo()
-
-    root = Tk()
-    root.resizable(0, 0)
-
-    def writeCoord():
-        """
-        Here in this function we get both coordinate and servo control, it is an ideal place to implement the controller
-        """
-        corrd_info = queue.get()
-
-        if corrd_info == 'nil': # Checks if the output is nil
-            print('cant fins the ball :(')
-        else:
-            print('The position of the ball : ', corrd_info[2])
-
-            if (-90 < corrd_info[0] < 90) and (-90 < corrd_info[1] < 90) and (-90 < corrd_info[2] < 90):
-
-                all_angle_assign(corrd_info[0],corrd_info[1],corrd_info[2])
-            else:
-                all_angle_assign(0,0,0)
-
-    def ballpos_to_servo_angle(x_cord, y_cord):
-        # convert the distance to center to angle.
-        x_ang = map_x_to_y(x_cord, x_min=28, x_max=-28, y_min=-20, y_max=20)
-        y_ang = map_x_to_y(y_cord, x_min=28, x_max=-28, y_min=-20, y_max=20)
-
-        # x and y angle(deg) in and servoangle out(rad)
-        servo_ang1, servo_ang2, servo_ang3 = kinematics(0, 22, y_ang, x_ang)
-        print("angle", servo_ang1, " ", (servo_ang2), " ", (servo_ang3))
-
-        return servo_ang1, servo_ang2, servo_ang3
-
-    def map_x_to_y(value, x_min, x_max, y_min, y_max):
-        return y_min + (((value - x_min) / (x_max - x_min)) * (y_max - y_min))
-    def filter_write_angle_servo(servo1_angle_deg, servo2_angle_deg, servo3_angle_deg):
-        if (-90 < servo1_angle_deg < 90) and (-90 < servo2_angle_deg < 90) and (-90 < servo3_angle_deg < 90):
-            write_servo(servo1_angle_deg, servo2_angle_deg, servo3_angle_deg)
-        else:
-            write_servo(0, 0, 0)
-    def write_arduino(data):
-        print('The angles send to the arduino : ', data)
-
-        arduino.write(bytes(data, 'utf-8'))
-
     def kinematics(Z, rotZdeg, rotYdeg, rotXdeg):
-        L = 24
+        L = 19
         R = 4
 
         # Convert degrees to radians
@@ -183,29 +130,74 @@ def servo_control(key2, queue):
 
         return angle1, angle2, angle3
 
+    root = Tk()
+
+    # root.resizable(0,0)
+
+    def map_x_to_y(value, x_min, x_max, y_min, y_max):
+        return y_min + (((value - x_min) / (x_max - x_min)) * (y_max - y_min))
+
+    def get_ball_pos():
+        cord_info = queue.get()
+        return cord_info
+
+    def P_Reg(pos_x, pos_y):  # out = kp*e   e = reff - pos
+        kp = 1.2
+        reff_val_x = 0
+        reff_val_y = 0
+        if (pos_x == 'nil') or (pos_y == 'nil'):
+            pos_x = 0
+            pos_y = 0
+
+        error_x = reff_val_x - pos_x
+        error_y = reff_val_y - pos_y
+        output_x = error_x * kp
+        output_y = error_y * kp
+        return output_x, output_y
+
+
+    def ballpos_to_servo_angle(x_cord, y_cord):
+        # convert the distance to center to angle.
+        x_ang = map_x_to_y(x_cord, x_min=28.0, x_max=-28.0, y_min=-20.0, y_max=20.0)
+        y_ang = map_x_to_y(y_cord, x_min=28.0, x_max=-28.0, y_min=-20.0, y_max=20.0)
+
+        # x and y angle(deg) in and servoangle out(rad)
+        servo_ang1, servo_ang2, servo_ang3 = kinematics(0, 22, y_ang, x_ang)
+        print("angle", servo_ang1, " ", (servo_ang2), " ", (servo_ang3))
+
+        return servo_ang1, servo_ang2, servo_ang3
+
+    def filter_write_angle_servo(servo1_angle_deg, servo2_angle_deg, servo3_angle_deg):
+        if (-90 < servo1_angle_deg < 90) and (-90 < servo2_angle_deg < 90) and (-90 < servo3_angle_deg < 90):
+            write_servo(servo1_angle_deg, servo2_angle_deg, servo3_angle_deg)
+        else:
+            write_servo(0, 0, 0)
+
     def write_servo(ang1, ang2, ang3):
         angles: tuple = (round(ang1, 1),
                          round(ang2, 1),
-                         round(ang3, 1))
+                         round(ang3,1))
         write_arduino(str(angles))
 
-    kp = 1
-    ki = 1
-    kd = 3
-    reff_val_x = 0
-    reff_val_y = 0
+    def write_arduino(data):
+        print('The angles send to the arduino : ', data)
+        arduino.write(bytes(data, 'utf-8'))
+
+    kp = 0.53
+    ki = 0.315
+    kd = 0.25
+
     integral_error_x = 0
     integral_error_y = 0
     last_error_x = 0
     last_error_y = 0
     start_time = 0
 
-    def get_ball_pos():
-        cord_info = queue.get()
-        return cord_info
+
     while key2:
+
         cord_info = get_ball_pos()  # Ballpos
-        if cord_info == 'nil':
+        if cord_info =='nil':
             error_x = 0
             error_y = 0
             integral_error_x = 0
@@ -217,38 +209,50 @@ def servo_control(key2, queue):
         else:
             pos_x = cord_info[0]
             pos_y = cord_info[1]
+        reff_val_x = 0
+        reff_val_y = 0
+        dt = time.time()-start_time
 
-        dt = time.time() - start_time
-        # print(dt)
         error_x = reff_val_x - pos_x
         error_y = reff_val_y - pos_y
         integral_error_x += error_x * dt
         integral_error_y += error_y * dt
-        # print("integral error:  ", integral_error_x, "   ", integral_error_y)
+        #print("integral error:  ", integral_error_x, "   ", integral_error_y)
         deriv_error_x = (error_x - last_error_x) / dt
         deriv_error_y = (error_y - last_error_y) / dt
-        # print("deriv error:  ", deriv_error_x, "   ", deriv_error_y)
+        #print("deriv error:  ", deriv_error_x, "   ", deriv_error_y)
         last_error_x = error_x
         last_error_y = error_y
         output_x = error_x * kp + ki * integral_error_x + kd * deriv_error_x
         output_y = error_y * kp + ki * integral_error_y + kd * deriv_error_y
-        # print(output_x, "   ", output_y)
+        #print(output_x, "   ", output_y)
 
         servo_ang1, servo_ang2, servo_ang3 = ballpos_to_servo_angle(output_x, output_y)  # Ballpos to servo angle
         filter_write_angle_servo(servo_ang1, servo_ang2, servo_ang3)  # Servo angle to arduino
 
         start_time = time.time()
     root.mainloop()  # running loop
-    # running loop
+
 
 if __name__ == '__main__':
-
-    queue = Queue() # The queue is done inorder for the communication between the two processes.
-    key1 = 1 # just two dummy arguments passed for the processes
+    queue = Queue()  # The queue is done inorder for the communication between the two processes.
+    key1 = 1  # just two dummy arguments passed for the processes
     key2 = 2
-    p1 = mp.Process(target= ball_track, args=(key1, queue)) # initiate ball tracking process
-    p2 = mp.Process(target=servo_control,args=(key2, queue)) # initiate servo controls
+    p1 = mp.Process(target=ball_track, args=(key1, queue))  # initiate ball tracking process
+    p2 = mp.Process(target=servo_control, args=(key2, queue))  # initiate servo controls
     p1.start()
     p2.start()
     p1.join()
     p2.join()
+
+
+    test = [0,0,0]
+    teller = 0
+    test[teller]= verdi
+    teller+=1
+    if teller > 2
+        teller = 0
+
+    gjennomsnitt = sum(test)/len(teller)
+
+    gjennomsnitt til servo
