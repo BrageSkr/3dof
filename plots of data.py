ca115a155423393fa86a31ce81db2f25574ea0ba
Brageskrettingland <brageskrettingland@gmail.com>
2023-11-30 14:35:16 +0100

    code that works, and changes to the plotting

--- plots of data.py
+++ plots of data.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 # Define the CSV file and field names
 output_dir = 'Gen_Data'
 output_file = f'{output_dir}/saved_data.csv'
-fieldnames = ["num", "x", "y", "targetX","targetY", "errorX","errorY","errortot","PidX","PidY"]
+fieldnames = ["num", "x", "y", "targetX", "targetY", "errorX", "errorY", "tot_error", "PID_x", "PID_y"]
 
 # Read data using pandas
 data = pd.read_csv(output_file)
@@ -16,36 +16,59 @@ targetX_values = data['targetX']
 targetY_values = data['targetY']
 errorX_values = data['errorX']
 errorY_values = data['errorY']
-errortot_values = data['errortot']
-Pidx_values = data['PidX']
-Pidy_values = data['PidY']
+errortot_values = data['tot_error']
+Pidx_values = data['PID_x']
+Pidy_values = data['PID_y']
 
-# Plotting
-plt.figure(figsize=(18, 14))
-plt.grid()
-plt.style.use('ggplot')
+# Plotting individual figures
+plt.figure(figsize=(8, 6))
+
 # Plot x vs y
-plt.subplot(3, 1, 1)
 plt.plot(x_values, y_values, label='Position (x,y)')
 plt.plot(targetX_values, targetY_values, label='Target (x,y)')
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.legend()
+plt.title('Position (x,y) and Target (x,y)')
 
+# Show the plot
+plt.show()
+
+# Create a new figure for the next plot
+plt.figure(figsize=(8, 6))
 
 # Plot errorX vs errorY
-plt.subplot(3, 1, 2)
 plt.plot(errorX_values, errorY_values, label='Error (x,y)')
 plt.xlabel('ErrorX')
 plt.ylabel('ErrorY')
 plt.legend()
+plt.title('Error (x,y)')
 
-plt.subplot(3, 1, 3)
-plt.plot( errorX_values, label='ErrorX', color='green')
-plt.plot( errorY_values, label='ErrorY',color='blue')
+# Show the plot
+plt.show()
+
+# Create a new figure for the third plot
+plt.figure(figsize=(8, 6))
+
+# Plot errorX and errorY separately vs Time
+plt.plot(errorX_values, label='ErrorX', color='green')
+plt.plot(errorY_values, label='ErrorY', color='blue')
 plt.xlabel('Time')
 plt.ylabel('Error')
 plt.legend()
 plt.title('ErrorX and ErrorY vs Time')
-plt.tight_layout()
+
+# Show the plot
+plt.show()
+plt.figure(figsize=(8, 6), dpi=900)
+
+# Plot x vs y
+plt.plot(Pidx_values, label='Pådrag PID x')
+plt.plot(Pidy_values,  label='Pådrag PID Y')
+plt.xlabel('time')
+plt.ylabel('gain')
+plt.legend()
+plt.title('Effect of PID regulator ')
+
+# Show the plot
 plt.show()
